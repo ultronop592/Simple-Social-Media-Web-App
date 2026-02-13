9f409035e678bbb6ba99d99376ecae1b7158e323
ultronop492 <ultronop558@gmail.com>
2026-02-13 21:42:01 +0530

    update the main.py and render.yaml

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", port=8001, reload=False)
+    port = int(os.getenv("PORT", 8001))
+    uvicorn.run("app.app:app", host="0.0.0.0", port=port, reload=False)
